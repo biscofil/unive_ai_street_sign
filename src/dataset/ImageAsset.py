58597ddebe6073bad651200e07f1d0386212cca1
Filippo <filippo.bisconcin@gmail.com>
2020-12-16 15:47:57 +0100

    ImageAsset mocks image by generating and blurring noise

--- src/dataset/ImageAsset.py
+++ src/dataset/ImageAsset.py
@@ -1,4 +1,6 @@
-from PIL import Image
+import torch
+from PIL import Image, ImageFilter
+from torchvision import transforms
 
 
 class ImageAsset:
@@ -23,12 +25,14 @@ class ImageAsset:
             class_id=int(csv_img_row["ClassId"])
         )
 
-    def __init__(self, filename: str, cropping_rect, class_id: int, augmentation_operations: list = None):
+    def __init__(self, filename: str, cropping_rect, class_id: int, augmentation_operations: list = None,
+                 random_noise: bool = False):
         self.filename: str = filename
         self.augmentation_operations: list = [] if augmentation_operations is None else augmentation_operations
         # must have format X1,Y1,X2,Y2
         self.cropping_rect = cropping_rect
         self.class_id: int = class_id
+        self.random_noise: bool = random_noise
 
     def print(self):
         print("Image:")
@@ -37,14 +41,17 @@ class ImageAsset:
         print("\tClass:", self.class_id)
 
     def load_and_transform_image(self) -> Image:
-        # load image
-        image_filename = self.filename
-        image_data = Image.open(image_filename).convert('RGB')
-        # transform
-        augmentation_callbacks = self.augmentation_operations
-        if not isinstance(augmentation_callbacks, list):
-            augmentation_callbacks = [augmentation_callbacks]
-        for callback in augmentation_callbacks:
-            # call augmentation callback
-            image_data = callback(image_data)
-        return image_data
+        if self.random_noise:
+            return (transforms.ToPILImage())(torch.rand(3, 30, 30)).filter(ImageFilter.GaussianBlur(radius=1))
+        else:
+            # load image
+            image_filename = self.filename
+            image_data = Image.open(image_filename).convert('RGB')
+            # transform
+            augmentation_callbacks = self.augmentation_operations
+            if not isinstance(augmentation_callbacks, list):
+                augmentation_callbacks = [augmentation_callbacks]
+            for callback in augmentation_callbacks:
+                # call augmentation callback
+                image_data = callback(image_data)
+            return image_data
